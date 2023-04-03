2f9333ad8c31a6181a2938b5cdd91ef12d3a14a2
Brandon Martel <brandon@heartex.com>
2023-04-03 20:16:06 +0000

    rename ls_lite_frontend to frontend

--- ls_frontend_lite/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    globals: {
-        process: true,
-        module: true,
-        require: true,
-        __dirname: true,
-    },
-    extends: ['plugin:@heartexlabs/frontend/recommended'],
-};
--- ls_frontend_lite/e2e/setup/feature-flags.js
+++
@@ -1,7 +0,0 @@
-/*
-  List of feature flags that should be switched on during every testing scenario
- */
-
-module.exports = {
-  fflag_feat_front_lsdv_4659_skipduplicates_060323_short: true,
-};
