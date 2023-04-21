32d7ce85f398873737fe4787c710d626dc4c43fa
Wesley Lima <wesley@heartex.com>
2023-04-21 12:26:07 -0400

    Added static file urls

--- ls_lite/urls.py
+++ ls_lite/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
 from comments import api
@@ -11,4 +13,4 @@ router.register(r"status", api.StatusAPI, basename="status")
 
 urlpatterns = [
     path('api/', include((router.urls, app_name), namespace="api")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
